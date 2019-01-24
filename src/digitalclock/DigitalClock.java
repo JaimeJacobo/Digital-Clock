d5cf18f0cc28eb25db4b2cca7dc7389c1dda236b
Jaime Jacobo <jaimejacobordv@gmail.com>
2019-01-24 01:29:49 -0500

    downloadng packages

--- src/digitalclock/DigitalClock.java
+++ src/digitalclock/DigitalClock.java
@@ -1,21 +1,32 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package digitalclock;
 
-/**
- *
- * @author jaimejacobo
- */
-public class DigitalClock {
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.Color;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
-    /**
-     * @param args the command line arguments
-     */
+
+
+
+public class DigitalClock extends JFrame {
+    
+    JLabel jlabClock ;
+    
+    public DigitalClock() {
+     jlabClock = new JLabel("");
+     setLayout(new FlowLayout());
+     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+     add(jlabClock);
+     setSize(1200,120);
+     setLocationRelativeTo(null);
+     setVisible(true);
+    }
+
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        new DigitalClock();
     }
     
 }
